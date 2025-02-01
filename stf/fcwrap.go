6a2a015c9f8276c32d5c13b5386c903cd5b66bad
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-02-01 12:38:18 +0100

    mc

--- stf/fcwrap.go
+++ stf/fcwrap.go
@@ -3,33 +3,52 @@ package stf
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"io"
 )
 
-type fswt struct {
-	wrapped func(any) any
+type fwStType struct {
+	is            InStream
+	wrappedCalled bool
+	wrappedResult any
 }
 
-func (f fswt) Start() error {
-	//TODO implement me
-	panic("implement me")
+func (sw *fwStType) Start() error {
+	sw.is.Start()
+	return nil
 }
 
-func (f fswt) Wait() error {
-	//TODO implement me
-	panic("implement me")
+func (sw *fwStType) Wait() error {
+	return nil
 }
 
-var fsw StartWaiter = &fswt{}
+var _ StartWaiter = &fwStType{}
 
 func NewFuncWrapper(ctx context.Context, wrapped func(any) any, rr io.Reader, wr io.Writer, opts ...FcOption) (Function, error) {
-	sw := &fswt{wrapped: wrapped}
+	sw := &fwStType{}
 	fc, err := NewFunction(ctx, sw, opts...)
 	if err != nil {
 		return nil, err
 	}
-	fc.AddInStream(rr, IstMaxNb(1), IstASet(json.Unmarshal, func(a any) error {
+	os, err := fc.AddOutStream(wr, OstMaxNb(1), OstAGet(
+		func() (any, error) {
+			if !sw.wrappedCalled {
+				return nil, errors.New("consume function not yet not called")
+			}
+			return sw.wrappedResult, nil
+		},
+		json.Marshal))
+	if err != nil {
+		return nil, err
+	}
+	is, err := fc.AddInStream(rr, IstMaxNb(1), IstASet(json.Unmarshal, func(ia any) error {
+		sw.wrappedResult = wrapped(ia)
+		os.Start()
 		return nil
 	}))
+	if err != nil {
+		return nil, err
+	}
+	sw.is = is
 	return fc, nil
 }
