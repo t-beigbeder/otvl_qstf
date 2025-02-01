2211785b436fa1692a54f8d77af6e8f67b390312
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-02-01 14:58:35 +0100

    tests ok

--- stf/fcwrap_test.go
+++ stf/fcwrap_test.go
@@ -1,40 +1,18 @@
 package stf
 
 import (
-	"bufio"
 	"bytes"
 	"context"
-	"encoding/binary"
-	"encoding/json"
 	"github.com/stretchr/testify/assert"
-	"io"
 	"testing"
+	"time"
 )
 
-type bufwc struct {
-	buf *bytes.Buffer
-	out *bufio.Writer
-}
-
-func (b bufwc) Write(p []byte) (n int, err error) {
-	return b.buf.Write(p)
-}
-
-func newBufwc() *bufwc {
-	var buf bytes.Buffer
-	return &bufwc{buf: &buf, out: bufio.NewWriter(&buf)}
-}
-
-var _ io.Writer = &bufwc{}
-
-func TestNewFuncWrapper(t *testing.T) {
-	bs, err := json.Marshal("value for test")
+func TestNewFuncWrapperBasic(t *testing.T) {
+	wbs, err := toJsonBytes("value for test")
 	assert.NoError(t, err)
-	wbs := make([]byte, len(bs)+4)
-	binary.BigEndian.PutUint32(wbs, uint32(len(bs)))
-	copy(wbs[4:], bs)
 	in := bytes.NewReader(wbs)
-	out := newBufwc()
+	out := newBufWr()
 	fcw, err := NewFuncWrapper(context.Background(),
 		func(a any) any {
 			return "response for " + a.(string)
@@ -43,8 +21,24 @@ func TestNewFuncWrapper(t *testing.T) {
 	assert.NoError(t, err)
 	err = fcw.Run()
 	assert.NoError(t, err)
-	obs := out.buf.Bytes()
-	var res any
-	json.Unmarshal(obs[4:], &res)
-	assert.Equal(t, "response for value for test", res)
+	a, err := fromJsonBytes(out)
+	assert.Equal(t, "response for value for test", a)
+}
+
+func TestNewFuncWrapperSlow(t *testing.T) {
+	wbs, err := toJsonBytes("value for test")
+	assert.NoError(t, err)
+	in := bytes.NewReader(wbs)
+	out := newBufWr()
+	fcw, err := NewFuncWrapper(context.Background(),
+		func(a any) any {
+			time.Sleep(time.Millisecond * 200)
+			return "response for " + a.(string)
+		},
+		in, out)
+	assert.NoError(t, err)
+	err = fcw.Run()
+	assert.NoError(t, err)
+	a, err := fromJsonBytes(out)
+	assert.Equal(t, "response for value for test", a)
 }
---
+++ stf/tests.go
@@ -0,0 +1,55 @@
+package stf
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+func toJsonBytes(a any) ([]byte, error) {
+	bs, err := json.Marshal("value for test")
+	if err != nil {
+		return nil, err
+	}
+	wbs := make([]byte, len(bs)+4)
+	binary.BigEndian.PutUint32(wbs, uint32(len(bs)))
+	copy(wbs[4:], bs)
+	return wbs, err
+}
+
+type bufWr struct {
+	buf *bytes.Buffer
+	out *bufio.Writer
+}
+
+var _ io.Writer = &bufWr{}
+
+func (b *bufWr) Write(p []byte) (n int, err error) {
+	return b.buf.Write(p)
+}
+
+func newBufWr() io.Writer {
+	var buf bytes.Buffer
+	return &bufWr{buf: &buf, out: bufio.NewWriter(&buf)}
+}
+
+func fromJsonBytes(wr io.Writer) (any, error) {
+	o, ok := wr.(*bufWr)
+	if !ok {
+		return nil, fmt.Errorf("expected bufWr got %T", wr)
+	}
+	obs := o.buf.Bytes()
+	bln := binary.BigEndian.Uint32(obs[0:4])
+	if bln != uint32(len(obs)-4) {
+		return nil, fmt.Errorf("invalid number of bytes read: expected %d got %d", len(obs)-4, bln)
+	}
+	var a any
+	err := json.Unmarshal(obs[4:], &a)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
