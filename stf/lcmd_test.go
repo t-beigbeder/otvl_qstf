bd6fd2a6ade216456ffc3e3825c3f1e1551b846a
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-02-02 14:49:50 +0100

    mt

--- stf/lcmd_test.go
+++ stf/lcmd_test.go
@@ -3,6 +3,7 @@ package stf
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -49,6 +50,85 @@ func TestNewLocalCommandIO(t *testing.T) {
 	res, err := fromBufWr(stdout)
 	require.NoError(t, err)
 	require.Equal(t, "hello world\n", string(res))
+	res, err = fromBufWr(stderr)
+	require.NoError(t, err)
+	require.Nil(t, res)
+}
+
+func TestNewLocalCommandStderr(t *testing.T) {
+	stdin := bytes.NewReader(nil)
+	stdout := newBufWr()
+	stderr := newBufWr()
+	cs := CommandSpec{
+		Cmd:  "sh",
+		Args: []string{"-c", "echo hello world >&2"},
+	}
+	lcfc, err := NewLocalCommand(
+		context.Background(),
+		cs,
+		stdin, stdout, stderr,
+		FcName("TestNewLocalCommandIO"))
+	require.NoError(t, err)
+	err = lcfc.Run()
+	require.NoError(t, err)
+	res, err := fromBufWr(stdout)
+	require.NoError(t, err)
+	require.Nil(t, res)
+	res, err = fromBufWr(stderr)
+	require.NoError(t, err)
+	require.Equal(t, "hello world\n", string(res))
+}
+
+func TestNewLocalCommandLarge(t *testing.T) {
+	instr := ""
+	for i := 0; i < 10000; i++ {
+		instr += fmt.Sprintf("hello world #%d\n", i)
+	}
+	stdin := bytes.NewReader([]byte(instr))
+	stdout := newBufWr()
+	stderr := newBufWr()
+	cs := CommandSpec{
+		Cmd: "cat",
+	}
+	lcfc, err := NewLocalCommand(
+		context.Background(),
+		cs,
+		stdin, stdout, stderr,
+		FcName("TestNewLocalCommandLarge"))
+	require.NoError(t, err)
+	err = lcfc.Run()
+	require.NoError(t, err)
+	res, err := fromBufWr(stdout)
+	require.NoError(t, err)
+	require.Equal(t, instr, string(res))
+	res, err = fromBufWr(stderr)
+	require.NoError(t, err)
+	require.Nil(t, res)
+}
+
+func TestNewLocalCommandLong(t *testing.T) {
+	instr := ""
+	for i := 0; i < 10000; i++ {
+		instr += fmt.Sprintf("hello world #%d\n", i)
+	}
+	stdin := bytes.NewReader([]byte(instr))
+	stdout := newBufWr()
+	stderr := newBufWr()
+	cs := CommandSpec{
+		Cmd:  "sh",
+		Args: []string{"-c", "cat && sleep 0.2"},
+	}
+	lcfc, err := NewLocalCommand(
+		context.Background(),
+		cs,
+		stdin, stdout, stderr,
+		FcName("TestNewLocalCommandLong"))
+	require.NoError(t, err)
+	err = lcfc.Run()
+	require.NoError(t, err)
+	res, err := fromBufWr(stdout)
+	require.NoError(t, err)
+	require.Equal(t, instr, string(res))
 	res, err = fromBufWr(stderr)
 	require.NoError(t, err)
 	require.Nil(t, res)
