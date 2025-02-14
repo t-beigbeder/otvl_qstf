774c4bed8e9758b3082f1525926569cfe925bef7
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-02-14 22:02:26 +0100

    implemented local command function, to be tested with client api to be defined

--- appctl/lcmd.go
+++ appctl/lcmd.go
@@ -2,10 +2,15 @@ package appctl
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/t-beigbeder/otvl_qstf/stf"
 	"os/exec"
 )
+
+type CommandExitStatus struct {
+	ExitCode int `json:"exit-code"`
+}
 
 type lcStartWait struct {
 	cmd *exec.Cmd
@@ -18,16 +23,37 @@ func (sw *lcStartWait) Start(ctx context.Context) error {
 	if len(fc.GetInStreams()) != 1 || len(fc.GetOutStreams()) != 2 {
 		return fmt.Errorf("invalid in/out streams count %d/%d", len(fc.GetInStreams()), len(fc.GetOutStreams()))
 	}
-	//sw.cmd = exec.CommandContext(ctx)
-	//return sw.cmd.Start()
+	aInPl, ok := CurrentValues(ctx)["in-pl"]
+	if !ok {
+		return fmt.Errorf("in-pl not found in context")
+	}
+	cs, ok := aInPl.(*stf.CommandSpec)
+	if !ok {
+		return fmt.Errorf("in-pl type %T expected %T", aInPl, &stf.CommandSpec{})
+	}
+	sw.cmd = exec.CommandContext(ctx, cs.Cmd, cs.Args...)
+	sw.cmd.Env = cs.Env
+	sw.cmd.Dir = cs.Dir
+	sw.cmd.Stdin = fc.GetInStreams()[0]
+	sw.cmd.Stdout = fc.GetOutStreams()[1]
+	sw.cmd.Stderr = fc.GetOutStreams()[2]
+	err := sw.cmd.Start()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (sw *lcStartWait) Wait(_ context.Context) error {
-	//err := sw.cmd.Wait()
-	//if err != nil {
-	//	return err
-	//}
+func (sw *lcStartWait) Wait(ctx context.Context) error {
+	err := sw.cmd.Wait()
+	if err != nil {
+		ee := exec.ExitError{}
+		if errors.As(err, &ee) {
+			CurrentValues(ctx)["out-pl"] = &CommandExitStatus{ExitCode: ee.ExitCode()}
+			return nil
+		}
+		return err
+	}
 	return nil
 }
 
@@ -43,7 +69,12 @@ func LocalCommandFuncDeclarer(fName, isName, osName, esName string) func(*Functi
 					{StreamDesc: StreamDesc{Name: osName}},
 					{StreamDesc: StreamDesc{Name: esName}},
 				},
+				Wrapper: WrapperDesc{InMarshaller: MarshalJson, OutMarshaller: MarshalJson},
 			},
-			nil, &lcStartWait{}, nil)
+			&stf.WrappedFunction{
+				InputTemplate:  FactoryFor[stf.CommandSpec],
+				OutputTemplate: FactoryFor[CommandExitStatus],
+			},
+			&lcStartWait{}, nil)
 	}
 }
