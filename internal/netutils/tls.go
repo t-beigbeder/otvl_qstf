55dc3598669500406b8101eed2def8e123ae3193
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-03-08 17:31:32 +0100

    mtls

--- internal/netutils/tls.go
+++ internal/netutils/tls.go
@@ -9,6 +9,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"math/big"
+	"net"
 	"time"
 )
 
@@ -58,6 +59,107 @@ func SelfSigned(host string) (*tls.Certificate, error) {
 	}
 	cert, err := tls.X509KeyPair(certPem.Bytes(), keyPem.Bytes())
 	return &cert, err
+}
+
+func getRandSN() (*big.Int, error) {
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, err
+	}
+	return serialNumber, nil
+}
+
+func NewCaCert() (*x509.CertPool, *x509.Certificate, *rsa.PrivateKey, error) {
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour)
+	serialNumber, err := getRandSN()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	caCert := &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"otvl"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	caBytes, err := x509.CreateCertificate(rand.Reader, caCert, caCert, &caPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	caPEM := new(bytes.Buffer)
+	pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	certpool := x509.NewCertPool()
+	certpool.AppendCertsFromPEM(caPEM.Bytes())
+	return certpool, caCert, caPrivKey, nil
+}
+
+func NewCert(hosts []string, caCert *x509.Certificate, caPrivKey *rsa.PrivateKey) (*tls.Certificate, error) {
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour)
+	serialNumber, err := getRandSN()
+	if err != nil {
+		return nil, err
+	}
+	cert := &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"otvl"},
+		},
+		IPAddresses: []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		NotBefore:   notBefore,
+		NotAfter:    notAfter,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
+	if len(hosts) > 0 {
+		for _, h := range hosts {
+			if ip := net.ParseIP(h); ip != nil {
+				cert.IPAddresses = append(cert.IPAddresses, ip)
+			} else {
+				cert.DNSNames = append(cert.DNSNames, h)
+			}
+		}
+		cert.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+	} else {
+		cert.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth}
+	}
+	certPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, caCert, &certPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return nil, err
+	}
+	certPEM := new(bytes.Buffer)
+	pem.Encode(certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+	certPrivKeyPEM := new(bytes.Buffer)
+	pem.Encode(certPrivKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(certPrivKey),
+	})
+	tlsCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivKeyPEM.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return &tlsCert, nil
 }
 
 func NextProtosFor(alpn string) []string {
--- internal/netutils/tls_test.go
+++ internal/netutils/tls_test.go
@@ -3,6 +3,7 @@ package netutils
 import (
 	"context"
 	"crypto/tls"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"testing"
 	"time"
@@ -28,6 +29,40 @@ func TestSelfSigned(t *testing.T) {
 	hc := http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	get, err := hc.Get("https://localhost:9443")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if get.StatusCode != http.StatusNotFound {
+		t.Fatalf("status code %d", get.StatusCode)
+	}
+}
+
+func TestNewServerCert(t *testing.T) {
+	pool, caCert, caPrik, err := NewCaCert()
+	require.NoError(t, err)
+	_, _, _ = pool, caCert, caPrik
+	tlsCert, err := NewCert([]string{"0.0.0.0", "localhost"}, caCert, caPrik)
+	require.NoError(t, err)
+	_ = tlsCert
+
+	cfg := &tls.Config{Certificates: []tls.Certificate{*tlsCert}}
+	srv := &http.Server{
+		Addr:         "0.0.0.0:9443",
+		TLSConfig:    cfg,
+		ReadTimeout:  time.Minute,
+		WriteTimeout: time.Minute,
+	}
+	go func() {
+		_ = srv.ListenAndServeTLS("", "")
+	}()
+	defer func() { _ = srv.Shutdown(context.TODO()) }()
+	time.Sleep(200 * time.Millisecond)
+	hc := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{RootCAs: pool},
 		},
 	}
 	get, err := hc.Get("https://localhost:9443")
